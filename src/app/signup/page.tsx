783084c1b6e29d7256ce0ae0b80817e05ccba8f8
Yuka Kato <tappi_28@yahoo.co.jp>
2025-05-06 03:14:51 +0900

    ユーザー登録画面実装

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../utils/supabase";
+
+//Supabaseを使った新規ユーザー登録ページ
+const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `http://localhost:3000/login`, //登録メールのリンクをクリックしたあとに遷移するURL
+      },
+    });
+    if (error) {
+      alert("登録に失敗しました");
+    } else { //成功時は入力欄をリセットしてメッセージ送信
+      setEmail("");
+      setPassword("");
+      alert("確認メールを送信しました");
+    }
+  };
+
+  return (
+    <div className="flex justify-center pt-[140px]">
+      <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-[400px]">
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            メールアドレス
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="name@company.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            パスワード
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            登録
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Page;
