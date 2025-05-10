082ec55f33b407db9d47e9c0eaf9e8b13d0ada9f
Yuka Kato <tappi_28@yahoo.co.jp>
2025-05-10 13:59:38 +0900

    ImageタグにSupabaseドメイン追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["arbgzvvwhnfhnrddujsm.supabase.co"], //Supabaseのドメイン追加
     remotePatterns: [
+      { protocol: "https", hostname: "arbgzvvwhnfhnrddujsm.supabase.co" }, //Supabaseのドメイン追加
       { protocol: "https", hostname: "placehold.jp" },
       { protocol: "https", hostname: "images.microcms-assets.io" },
     ],
